4a2547e7c5dcbd777e2304d34488dab65d7a51df
Sergey <alexroverandom@gmail.com>
2015-03-14 19:08:35 +0200

    first commit
    initialize catalog

---
+++ js/carousel.js
@@ -0,0 +1,25 @@
+﻿var Carousel = function () {
+	var $carousel = $('.jPageTitle').removeClass('page_title').addClass('carousel');
+	$carousel.removeAttr('style');
+	var slidePhotosArray = [];
+	var $x = $('.jSlideImg');
+	for (var i = 0; i < $x.length; i++) {
+		slidePhotosArray.push($x[i]);
+	}
+	$carousel.children().remove();
+	for (var j = 0; j < slidePhotosArray.length; j++) {
+		$(slidePhotosArray[j]).appendTo($carousel);
+	}
+	$('body').css('overflow-x', 'hidden');
+
+	$('.carousel').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		slide: 'div',
+		cssEase: 'linear',
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+}
