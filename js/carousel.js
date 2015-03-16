a65cfc3a7c1fc20c022e5d45243edd6196492529
Sergey <alexroverandom@gmail.com>
2015-03-17 01:33:40 +0200

    night commit of small piece of code
    one half of html is ready (what is the simplest)

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-﻿var Carousel = function () {
-	var $carousel = $('.jPageTitle').removeClass('page_title').addClass('carousel');
-	$carousel.removeAttr('style');
-	var slidePhotosArray = [];
-	var $x = $('.jSlideImg');
-	for (var i = 0; i < $x.length; i++) {
-		slidePhotosArray.push($x[i]);
-	}
-	$carousel.children().remove();
-	for (var j = 0; j < slidePhotosArray.length; j++) {
-		$(slidePhotosArray[j]).appendTo($carousel);
-	}
-	$('body').css('overflow-x', 'hidden');
-
+var Carousel = function () {
 	$('.carousel').slick({
 		dots: true,
 		infinite: true,
@@ -23,30 +9,4 @@
 		autoplay: true,
 		autoplaySpeed: 2000
 	});
-=======
-﻿var Carousel = function () {
-	var $carousel = $('.jPageTitle').removeClass('page_title').addClass('carousel');
-	$carousel.removeAttr('style');
-	var slidePhotosArray = [];
-	var $x = $('.jSlideImg');
-	for (var i = 0; i < $x.length; i++) {
-		slidePhotosArray.push($x[i]);
-	}
-	$carousel.children().remove();
-	for (var j = 0; j < slidePhotosArray.length; j++) {
-		$(slidePhotosArray[j]).appendTo($carousel);
-	}
-	$('body').css('overflow-x', 'hidden');
-
-	$('.carousel').slick({
-		dots: true,
-		infinite: true,
-		speed: 500,
-		fade: true,
-		slide: 'div',
-		cssEase: 'linear',
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
->>>>>>> 4a2547e7c5dcbd777e2304d34488dab65d7a51df
 }
