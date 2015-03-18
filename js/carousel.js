cc55be6135ecd39895e0e538e59dadaf41bc1411
Sergey <alexroverandom@gmail.com>
2015-03-19 01:28:28 +0200

    complited banners section
    there are much works still must be done

--- js/carousel.js
+++ js/carousel.js
@@ -9,4 +9,36 @@ var Carousel = function () {
 		autoplay: true,
 		autoplaySpeed: 2000
 	});
-}
+    
+    $(".jServiceItem").on("mouseenter", "a", function(){
+        $(this).parent().find("a").css({
+            "color": "yellow",
+            "cursor": "pointer"
+        });
+    });
+    
+    $(".jServiceItem").on("mouseout", "a", function(){
+        $(this).parent().find("a").css({
+            "color": "white",
+            "cursor": "auto"
+        });
+    });
+    
+    $(".jDescription").hide();
+    $(".jPanel.active").find(".jDescription").show();
+    $(".jToggle").on("click", function(){
+        if (!$(this).parent().parent().hasClass("active")) {
+            $(".jPanel").removeClass("active");
+            $(".jToggle").html("+");
+            $(this).parent().parent().addClass("active")
+            $(this).html("-");
+            var descBlocks = $(".jDescription");
+            descBlocks.slideUp();
+            $(this).parent().next().slideDown();
+        } else {
+            $(this).parent().parent().removeClass("active");
+            $(this).html("+");
+            $(this).parent().next().slideUp();
+        }
+    });
+};
